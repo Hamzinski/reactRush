11c67f5f22a59bfe3dd2700cebb295015ffbb13f
Muhammed Hamza Toptancı <toptanci.hamza@gmail.com>
2024-07-30 16:52:58 +0300

    lifting_state_up

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Hamz" body="React refresherr!" />
-        <Post author="Hakotaktak" body="React refresherr!" />
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Hakotaktak" body="React is sugoii!" />
       </ul>
     </>
   );
