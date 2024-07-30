a4e04df222c74318f55028ba050469e4d2f05c9c
Muhammed Hamza Toptancı <toptanci.hamza@gmail.com>
2024-07-30 16:32:11 +0300

    working_with_state

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import NewPost from "./NewPost";
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Hamz" body="React refresherr!" />
-      <Post author="Hakotaktak" body="React refresherr!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Hamz" body="React refresherr!" />
+        <Post author="Hakotaktak" body="React refresherr!" />
+      </ul>
+    </>
   );
 }
 
