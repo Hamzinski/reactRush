e9c8f9cc2294ae5963bd1dcd35e2e700513e98e2
Muhammed Hamza Toptancı <toptanci.hamza@gmail.com>
2024-10-03 15:55:08 +0300

    -Adding a Backend to the React SPA -Sending a POST HTTP Request -Handling Side Effects with useEffect()

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 function PostsList({ isPosting, onStopPosting }) {
-  const [post, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
@@ -17,9 +34,19 @@ function PostsList({ isPosting, onStopPosting }) {
         </Modal>
       )}
 
-      <ul className={classes.posts}>
-        <Post author="Hakotaktak" body="React is sugoii!" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
