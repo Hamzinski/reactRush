4f4077562db2624eee4c7e613def5101f1399ec5
Muhammed Hamza Toptancı <toptanci.hamza@gmail.com>
2024-10-03 10:36:02 +0300

    State & Conditional Content

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,9 +2,15 @@ import React, { useState } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -15,10 +21,16 @@ function PostsList() {
 
   return (
     <>
-      <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
-      />
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          {" "}
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Hakotaktak" body="React is sugoii!" />
